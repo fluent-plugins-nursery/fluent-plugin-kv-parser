4cc5f6cc03d29f00231adbe50d52a8a8f0179136
okkez <okkez000@gmail.com>
2017-12-22 15:33:02 +0900

    Rewrite test

--- test/test_kv_parser.rb
+++ test/test_kv_parser.rb
@@ -1,61 +1,64 @@
 require 'fluent/test'
-require 'fluent/parser'
+require 'fluent/test/driver/parser'
 require 'fluent/plugin/parser_kv'
 
 class KVParserTest < ::Test::Unit::TestCase
-  include ParserTest
-
-  def test_basic
-    parser = Fluent::TextParser::KVParser.new
-    parser.configure
-    parser.parse("k1=v1 k2=v2") {|_, v| assert_equal({"k1"=>"v1", "k2"=>"v2"}, v)}
-    parser.parse("k1=v1    k2=v2") {|_, v| assert_equal({"k1"=>"v1", "k2"=>"v2"}, v)}
-    parser.parse("k2=v2 k1=v1") {|_, v| assert_equal({"k1"=>"v1", "k2"=>"v2"}, v)}
-    parser.parse("k2=v2\tk1=v1") {|_, v| assert_equal({"k1"=>"v1", "k2"=>"v2"}, v)}
-    parser.parse("k2=v2\t k1=v1") {|_, v| assert_equal({"k1"=>"v1", "k2"=>"v2"}, v)}
+  def setup
+    Fluent::Test.setup
   end
 
-  def test_with_types
-    parser = Fluent::TextParser::KVParser.new
-    parser.configure("types" => "k1:integer")
-    parser.parse("k1=100") {|_, v| assert_equal(100, v["k1"])}
+  def create_driver(conf={})
+    Fluent::Test::Driver::Parser.new(Fluent::Plugin::KVParser).configure(conf)
   end
 
-  def test_with_time
-    parser = Fluent::TextParser::KVParser.new
-    parser.configure("types" => "time:time")
-    parser.parse("k1=foo time=1970-01-01T01:00:00") {|time, v|
+  data("single space" => ["k1=v1 k2=v2", { "k1" => "v1", "k2" => "v2" }],
+       "multiple space" => ["k1=v1    k2=v2", { "k1" => "v1", "k2" => "v2" }],
+       "reverse" => ["k2=v2 k1=v1", { "k1" => "v1", "k2" => "v2" }],
+       "tab" => ["k2=v2\tk1=v1", { "k1" => "v1", "k2" => "v2" }],
+       "tab and space" => ["k2=v2\t k1=v1", { "k1" => "v1", "k2" => "v2" }])
+  test "parse" do |(text, expected)|
+    d = create_driver
+    d.instance.parse(text) do |_time, record|
+      assert_equal(expected, record)
+    end
+  end
+
+  test "parse with types" do
+    d = create_driver("types" => "k1:integer")
+    d.instance.parse("k1=100") do |_time, record|
+      assert_equal({ "k1" => 100 }, record)
+    end
+  end
+
+  test "parse with time" do
+    d = create_driver("types" => "time:time")
+    d.instance.parse("k1=foo time=1970-01-01T01:00:00") do |time, reocrd|
       assert_equal(3600, time)
-      assert_equal("foo", v["k1"])
-    }
+      assert_equal({ "k1" => "foo" }, record)
+    end
   end
 
-  def test_with_custom_time_key
-    parser = Fluent::TextParser::KVParser.new
-    parser.configure("time_key" => "my_time", "types" => "my_time:time")
-    parser.parse("k1=foo my_time=1970-01-01T01:00:00") {|time, v|
+  test "parse with custom time_key" do
+    d = create_driver("time_key" => "my_time", "types" => "my_time:time")
+    d.instance.parse("k1=foo my_time=1970-01-01T01:00:00") do |time, record|
       assert_equal(3600, time)
-      assert_equal("foo", v["k1"])
-    }
+      assert_equal({ "k1" => "foo" }, record)
+    end
   end
 
-  def test_custom_delimiter
-    parser = Fluent::TextParser::KVParser.new
-    parser.configure("kv_delimiter" => "|")
-    parser.parse("k1=v1|k2=v2") {|_, v| assert_equal({"k1"=>"v1", "k2"=>"v2"}, v)}
-    parser.configure("kv_delimiter" => "/[@ ]/")
-    parser.parse("k1=v1@k2=v2 k3=v3") {|_, v|
-      assert_equal({"k1"=>"v1", "k2"=>"v2", "k3"=>"v3"}, v)
-    }
+  data("pipe" => ["|", "k1=v1|k2=v2", {"k1" => "v1", "k2" => "v2" }],
+       "regexp" => ["/[@ ]/", "k1=v1@k2=v2 k3=v3", { "k1" => "v1", "k2" => "v2", "k3" => "v3" }])
+  test "parse with custom kv_delimiter" do |(delimiter, text, expected)|
+    d = create_driver("kv_delimiter" => delimiter)
+    d.instance.parse(text) do |_time, record|
+      assert_equal(expected, record)
+    end
   end
 
-  def test_custom_kv_char
-    parser = Fluent::TextParser::KVParser.new
-    parser.configure("kv_char" => "#")
-    parser.parse("k1#v1 k2#v2") {|_, v| assert_equal({"k1"=>"v1", "k2"=>"v2"}, v)}
-  end
-
-  def test_types_param
-    parser = Fluent::TextParser::KVParser.new
+  test "parse with custom kv_char" do
+    d = create_driver("kv_char" => "#")
+    d.instance.parse("k1#v1 k2#v2") do |_time, record|
+      assert_equal({ "k1" => "v1", "k2" => "v2" }, record)
+    end
   end
 end
