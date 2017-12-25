7849645d99e9553fd88a71ee956203a7b3bb3d57
Kenji Okimoto <okimoto@clear-code.com>
2017-12-25 11:10:53 +0900

    Use test driver for Fluentd v0.12.x

--- test/test_kv_parser.rb
+++ test/test_kv_parser.rb
@@ -4,9 +4,22 @@ require 'fluent/plugin/parser_kv'
 
 class KVParserTest < ::Test::Unit::TestCase
 
+  def create_driver(conf = {})
+    Fluent::Test::ParserTestDriver.new(Fluent::TextParser::KVParser).configure(conf)
+  end
+
+  def setup
+    @timezone = ENV["TZ"]
+    ENV["TZ"] = "UTC"
+  end
+
+  def teardown
+    ENV["TZ"] = @timezone
+  end
+
   def test_basic
-    parser = Fluent::TextParser::KVParser.new
-    parser.configure
+    d = create_driver
+    parser = d.instance
     parser.parse("k1=v1 k2=v2") {|_, v| assert_equal({"k1"=>"v1", "k2"=>"v2"}, v)}
     parser.parse("k1=v1    k2=v2") {|_, v| assert_equal({"k1"=>"v1", "k2"=>"v2"}, v)}
     parser.parse("k2=v2 k1=v1") {|_, v| assert_equal({"k1"=>"v1", "k2"=>"v2"}, v)}
@@ -15,14 +28,14 @@ class KVParserTest < ::Test::Unit::TestCase
   end
 
   def test_with_types
-    parser = Fluent::TextParser::KVParser.new
-    parser.configure("types" => "k1:integer")
+    d = create_driver("types" => "k1:integer")
+    parser = d.instance
     parser.parse("k1=100") {|_, v| assert_equal(100, v["k1"])}
   end
 
   def test_with_time
-    parser = Fluent::TextParser::KVParser.new
-    parser.configure("types" => "time:time")
+    d = create_driver("types" => "time:time")
+    parser = d.instance
     parser.parse("k1=foo time=1970-01-01T01:00:00") {|time, v|
       assert_equal(3600, time)
       assert_equal("foo", v["k1"])
@@ -30,8 +43,8 @@ class KVParserTest < ::Test::Unit::TestCase
   end
 
   def test_with_custom_time_key
-    parser = Fluent::TextParser::KVParser.new
-    parser.configure("time_key" => "my_time", "types" => "my_time:time")
+    d = create_driver("time_key" => "my_time", "types" => "my_time:time")
+    parser = d.instance
     parser.parse("k1=foo my_time=1970-01-01T01:00:00") {|time, v|
       assert_equal(3600, time)
       assert_equal("foo", v["k1"])
@@ -39,23 +52,22 @@ class KVParserTest < ::Test::Unit::TestCase
   end
 
   def test_custom_delimiter
-    parser = Fluent::TextParser::KVParser.new
-    parser.configure("kv_delimiter" => "|")
+    d = create_driver("kv_delimiter" => "|")
+    parser = d.instance
     parser.parse("k1=v1|k2=v2") {|_, v| assert_equal({"k1"=>"v1", "k2"=>"v2"}, v)}
-    parser.configure("kv_delimiter" => "/[@ ]/")
+  end
+
+  def test_custom_delimiter2
+    d = create_driver("kv_delimiter" => "/[@ ]/")
+    parser = d.instance
     parser.parse("k1=v1@k2=v2 k3=v3") {|_, v|
       assert_equal({"k1"=>"v1", "k2"=>"v2", "k3"=>"v3"}, v)
     }
   end
 
   def test_custom_kv_char
-    parser = Fluent::TextParser::KVParser.new
-    parser.configure("kv_char" => "#")
+    d = create_driver("kv_char" => "#")
+    parser = d.instance
     parser.parse("k1#v1 k2#v2") {|_, v| assert_equal({"k1"=>"v1", "k2"=>"v2"}, v)}
   end
-
-  def test_types_param
-    parser = Fluent::TextParser::KVParser.new
-  end
-
 end
